35212707fc1e2b2f09a9a60a001f831e681b6e00
Pavel <wtdtl07@gmail.com>
2025-11-18 18:32:22 +0300

    feat(blog): implement full CRUD with CBV

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+# blog/views.py
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import BlogPost
+from .forms import BlogPostForm
 
-# Create your views here.
+
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return BlogPost.objects.filter(is_published=True)
+
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save()
+
+        # Дополнительное задание: отправка email при 100 просмотрах
+        if obj.views_count == 100:
+            # Здесь будет код отправки email
+            pass
+
+        return obj
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    form_class = BlogPostForm
+    template_name = 'blog/blog_form.html'
+    success_url = reverse_lazy('blog_list')
+
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    form_class = BlogPostForm
+    template_name = 'blog/blog_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('blog_detail', kwargs={'pk': self.object.pk})
+
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/blog_confirm_delete.html'
+    success_url = reverse_lazy('blog_list')
