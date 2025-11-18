eb23590cffba5e16e4ee913cd87eed1bb19f4279
Pavel <wtdtl07@gmail.com>
2025-11-18 18:12:16 +0300

    refactor(catalog): convert FBV to CBV for all views

--- catalog/forms.py
+++ catalog/forms.py
@@ -5,3 +5,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'image', 'category', 'price']
+
+class FeedbackForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),  # ← новая строка
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
