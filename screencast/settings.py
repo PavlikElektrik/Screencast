68f5fe151ce71b8180efb4b23332635d6bc67f70
Pavel <wtdtl07@gmail.com>
2025-11-30 20:43:48 +0300

    404 fix

--- screencast/settings.py
+++ screencast/settings.py
@@ -74,10 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'screencast.wsgi.application'
-AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -138,3 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # screencast/settings.py
 ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'admin@example.com')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+WSGI_APPLICATION = 'screencast.wsgi.application'
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'users:login'
